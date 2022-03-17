351bbb092c75eeee5c6bae06e0b94c6433b4d3f8
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2022-03-17 02:07:46 +0100

    Update home

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, heatmap
+from .apps import home, heatmap
 
 st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
