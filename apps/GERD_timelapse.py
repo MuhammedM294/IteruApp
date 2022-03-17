71a2cfa4168f8c1dc87adff4bce12302dd302dfe
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2022-03-17 23:16:14 +0100

    Update

--- apps/GERD_timelapse.py
+++ apps/GERD_timelapse.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from iteru import Map
 
 
 def app():
@@ -8,12 +9,12 @@ def app():
 
     with row1_col1:
 
-        '''    m = Map()
-           m.center = (10.69399656037844, 35.22541966722389)
-           m.zoom = 10
-           map_layers = m.layers
-           m.remove_layer(map_layers[1])
-           m.to_streamlit(height=570) '''
+        m = Map()
+        m.center = (10.69399656037844, 35.22541966722389)
+        m.zoom = 10
+        map_layers = m.layers
+        m.remove_layer(map_layers[1])
+        m.to_streamlit(height=570)
 
     with row1_col2:
         with st.expander("Customize timelapse"):
