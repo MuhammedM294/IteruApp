f99d11c2fe0c1912f1ea9d4badcf54bd53434a20
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2022-03-17 13:46:36 +0100

    update

--- apps/GERD_timelapse.py
+++ apps/GERD_timelapse.py
@@ -1,11 +1,12 @@
 import streamlit as st
+from iteru import *
 
 
 def app():
 
     st.title("Create Sentinel-1 SAR GRD Timelapse for GERD")
     row1_col1, row1_col2 = st.columns([2, 1])
-    from iteru import Map
+
     with row1_col1:
 
         m = Map()
@@ -14,6 +15,27 @@ def app():
         map_layers = m.layers
         m.remove_layer(map_layers[1])
         m.to_streamlit(height=570)
+
+        GERD_aoi = ee.Geometry.Polygon([[[
+            35.008243,
+            10.522199
+        ],
+            [
+            35.008243,
+            11.266588
+        ],
+            [
+            35.387092,
+            11.266588
+        ],
+            [
+            35.387092,
+            10.522199
+        ],
+            [
+            35.008243,
+            10.522199
+        ]]])
     with row1_col2:
         with st.expander("Customize timelapse"):
             st.write('test')
