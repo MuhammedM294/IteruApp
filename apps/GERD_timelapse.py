726146716fe340609e0bca3d1ecfb89d4b9c32ab
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2022-03-20 01:23:30 +0100

    U

--- apps/GERD_timelapse.py
+++ apps/GERD_timelapse.py
@@ -1,35 +1,27 @@
+from geemap import sentinel2_timelapse
 import streamlit as st
-from iteru import Map, GERD_SAR_timelaspe
 import base64
+from geemap import temp_file_path
+from iteru import GERD_aoi
 
 
 def app():
 
-    st.title("Create Sentinel-1 SAR GRD Timelapse for GERD")
     row1_col1, row1_col2 = st.columns([2, 1])
 
     with row1_col1:
 
-        m = Map()
-        m.center = (10.69399656037844, 35.22541966722389)
-        m.zoom = 10
-        map_layers = m.layers
-        m.remove_layer(map_layers[1])
-        m.to_streamlit(height=570)
+        with st.form('TEST'):
+            submitted = st.form_submit_button("Submit")
 
-    with row1_col2:
-        with st.expander("Customize timelapse"):
-
-            with st.form('TEST'):
-                submitted = st.form_submit_button("Submit")
-
-                if submitted:
-                    out_gif = GERD_SAR_timelaspe()
-                    file_ = open(out_gif, "rb")
-                    contents = file_.read()
-                    data_url = base64.b64encode(contents).decode("utf-8")
-                    file_.close()
-                    st.markdown(
-                        f'<img src="data:image/gif;base64,{data_url}>',
-                        unsafe_allow_html=True,
-                    )
+            if submitted:
+                out_gif = temp_file_path('.gif')
+                out_gif = sentinel2_timelapse(GERD_aoi, out_gif=out_gif)
+                file_ = open(out_gif, "rb")
+                contents = file_.read()
+                data_url = base64.b64encode(contents).decode("utf-8")
+                file_.close()
+                st.markdown(
+                    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+                    unsafe_allow_html=True,
+                )
--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-import PIL
 from geemap import sentinel2_timelapse
 import streamlit as st
 import base64
-from iteru import *
-
-
-from geemap import sentinel2_timelapse
+from geemap import temp_file_path
+from iteru import GERD_aoi
 row1_col1, row1_col2 = st.columns([2, 1])
 
 with row1_col1:
@@ -14,12 +11,13 @@ with row1_col1:
         submitted = st.form_submit_button("Submit")
 
         if submitted:
-            out_gif = GERD_SAR_timelaspe()
+            out_gif = temp_file_path('.gif')
+            out_gif = sentinel2_timelapse(GERD_aoi, out_gif=out_gif)
             file_ = open(out_gif, "rb")
             contents = file_.read()
             data_url = base64.b64encode(contents).decode("utf-8")
             file_.close()
             st.markdown(
-                f'<img src="data:image/gif;base64,{data_url}>',
+                f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
                 unsafe_allow_html=True,
             )
