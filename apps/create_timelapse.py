54c1052d717889c3f4a10c7745a5f0550b43b44c
Muhammed Abdelaal <muhammedaabdelaal@gmail.com>
2022-03-21 13:14:45 +0100

    update

--- apps/create_timelapse.py
+++ apps/create_timelapse.py
@@ -12,11 +12,11 @@ def app():
         'This app for creating the GERD timelapses from 2020-01-01 so far.')
 
     row1_col1, row1_col2 = st.columns([2, 1])
-    with row1_col1:
 
-        m = Map(zoom=10, center=(10.7, 35.2))
+    with row1_col1:
+        m = Map(zoom=10, center=(10.65, 35.2))
         m.remove_layer(m.layers[1])
-        m.to_streamlit(height=600)
+        m.to_streamlit()
 
     with row1_col2:
 
@@ -78,8 +78,6 @@ def app():
 
             submitted = st.form_submit_button("Submit")
             if submitted:
-                with st.spinner('Loading...'):
-                    time.sleep(10)
                 try:
                     out_gif = GERD_SAR_timelaspe(GERD_aoi,
                                                  startYear=start_date.year,
@@ -131,16 +129,10 @@ def app():
                         with row1_col1:
 
                             st.markdown(
-                                f'<img src="data:image/gif;base64,{data_url}>',
+                                f'<img src="data:image/gif;base64,{data_url}" alt="GERD gif">',
                                 unsafe_allow_html=True,
                             )
                             with row1_col2:
                                 st.success('Done!')
-                                st.download_button(
-                                    label="Download GIF",
-                                    data=out_gif,
-                                    file_name='GERD_timelapse.gif',
-                                    mime='application/octet-stream',
-                                )
                     except Exception:
                         st.error('Nope! Something went wrong!')
